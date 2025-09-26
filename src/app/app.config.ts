b0bfffd63a4345a05a329783ba7c7f239ac7b8ce
seb mac <sebastien.net@gmail.com>
2025-09-26 03:07:52 +0200

    update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,23 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+
+
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimationsAsync(),
+
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    })
   ]
 };
