53c9adc1c1d3328c5a0688206f0a6c674c792a75
seb mac <sebastien.net@gmail.com>
2025-09-29 07:41:14 +0200

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './home-page/home-page.component';
+import { AdminPage } from './about-page/admin-page.component';
 
-export const routes: Routes = [];
+// export const routes: Routes = [];
+
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomePage,
+  },
+  {
+    path: 'admin',
+    component: AdminPage,
+  },
+];
